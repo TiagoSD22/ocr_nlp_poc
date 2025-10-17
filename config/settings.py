3b30580226fd1d69731a2c181d723baa3113ef2b
Tiago Siqueira Dionizio <tiagosdionizio@gmail.com>
2025-10-17 13:35:05 -0300

    Add S3_EXTERNAL_ENDPOINT_URL setting
    Introduces S3_EXTERNAL_ENDPOINT_URL to support presigned URLs, allowing configuration of an external S3 endpoint separate from the internal one.

--- config/settings.py
+++ config/settings.py
@@ -44,6 +44,7 @@ KAFKA_BOOTSTRAP_SERVERS = os.getenv('KAFKA_BOOTSTRAP_SERVERS', 'localhost:9092')
 # S3 settings (LocalStack)
 AWS_ENDPOINT_URL = os.getenv('AWS_ENDPOINT_URL', 'http://localhost:4566')
 S3_ENDPOINT_URL = os.getenv('S3_ENDPOINT_URL', 'http://localhost:4566')
+S3_EXTERNAL_ENDPOINT_URL = os.getenv('S3_EXTERNAL_ENDPOINT_URL', 'http://localhost:4566')  # For presigned URLs
 S3_BUCKET_NAME = os.getenv('S3_BUCKET_NAME', 'certificate-documents')
 S3_ACCESS_KEY = os.getenv('S3_ACCESS_KEY', 'localstack')
 S3_SECRET_KEY = os.getenv('S3_SECRET_KEY', 'localstack')
